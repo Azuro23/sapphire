9af958ac12ffbe2876e107741a1a573f1c8e89f5
MichaelIgb <igbanugo.michael@yahoo.com>
2023-06-16 13:50:06 -0400

    syncing foundation changes

---
+++ project sapphire/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function About() {
+
+    return (
+        <Flex  width={'100vw'} height={'100vh'} placeContent={'center'} placeItems={'center'}>
+
+        </Flex>
+    )
+}
---
+++ project sapphire/src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+
+export default function Profile() {
+
+    return (
+        <Flex width={'100vw'} height={'100vh'} placeContent={'center'} placeItems={'center'}>
+
+        </Flex>
+    )
+}
