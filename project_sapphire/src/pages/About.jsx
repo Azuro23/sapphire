cfb038b8fbe9018d30881039c1b811a1e73d610a
MikellRozier <mikell.rozier@live.com>
2023-06-23 20:03:52 -0400

    content

--- project_sapphire/src/pages/About.jsx
+++ project_sapphire/src/pages/About.jsx
@@ -17,35 +17,35 @@ import { members } from './data'
 export default function About() {
 
     return (
-      <Flex backgroundColor={"gray.700"} >
-    <Container maxW={''} py={{ base: '16', md: '24', }} >
+      <Flex backgroundColor={"#062143"} bgGradient={90} >
+    <Container maxW={'med'} py={{ base: '16', md: '24', }} >
     <Stack spacing={{base: '12', md: '16',  }}  >
       <Stack spacing={{ base: '8',  md: '10', }} direction={{ base: 'column', lg: 'row',}}justify="space-between" >
         <Stack spacing="3" maxW="3xl">
-          <Text fontSize={{ base: 'sm', md: 'md',}} color="white" fontWeight="semibold">
+          <Text fontSize={{ base: 'sm', md: 'md',}} color="#FCB721" fontWeight="semibold">
             Welcome
           </Text>
           <Stack spacing={{ base: '4', md: '5', }} >
-            <Heading  size={{ base: 'sm', md: 'xl', }} color="white" >
+            <Heading  size={{ base: 'sm', md: 'xl', }} color="#FCB721" >
               Meet our team
             </Heading>
-            <Text fontSize={{ base: 'lg', md: 'xl',  }} color="white"  >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, accusantium!
+            <Text fontSize={{ base: 'lg', md: 'xl',  }} color="#FCB721"  >
+            Azuro is owned and run by a group of hands-on partners who actively work on each and every project. There is no B-team. Working with Azuro means that our entire leadership team is involved in the day-to-day of your project, providing a level of dedication and attention not found in a large firm.
             </Text>
           </Stack>
         </Stack>
         <Stack
           direction={{base: 'column-reverse', md: 'row', }} spacing="3" >
-          <Button variant="secondary" size="xl" color="white">
+          <Button colorScheme="purple">
             Contact us
           </Button>
-          <Button variant="primary" size="xl" color="white">
+          <Button >
             Join our team
           </Button>
         </Stack>
       </Stack>
       <SimpleGrid
-        columns={{ base: 1, md: 2,  lg: 3, }} columnGap="8" rowGap={{base: '10', lg: '16', }} color="white">
+        columns={{ base: 1, md: 2,  lg: 3, }} columnGap="8" rowGap={{base: '10', lg: '16', }} color="#FCB721">
         {members.map((member) => (
           <Stack key={member.name} spacing="4">
             <Stack spacing="5">
@@ -72,6 +72,7 @@ export default function About() {
       </SimpleGrid>
     </Stack>
   </Container>
+  
   </Flex>
     )
 }
