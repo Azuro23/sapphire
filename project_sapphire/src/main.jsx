a972ddb998b6536eb3b7a39894ad25373e64daba
shadmac <shadreka.mcallister@gmail.com>
2023-06-27 15:51:22 -0400

    Deleted project sapphire, manual merge of files into project_sapphire. GitHub merge corrections.

--- project_sapphire/src/main.jsx
+++ project_sapphire/src/main.jsx
@@ -1,23 +1,25 @@
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App.jsx';
+import { theme as proTheme } from '@chakra-ui/pro-theme';
+import { extendTheme, theme as baseTheme } from '@chakra-ui/react';
 
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
-import App from './App.jsx'
-import { theme as proTheme } from '@chakra-ui/pro-theme'
-import { extendTheme, theme as baseTheme } from '@chakra-ui/react'
+// Supports weights 100-900
+import '@fontsource-variable/inter';
 
 export const theme = extendTheme(
-  {
-    colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
-    fonts: {
-      heading: "'Inter Variable', -apple-system, system-ui, sans-serif",
-      body: "'Inter Variable', -apple-system, system-ui, sans-serif",
-    },
-  },
-  proTheme,
-)
+	{
+		colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+		fonts: {
+			heading: "'Inter Variable', -apple-system, system-ui, sans-serif",
+			body: "'Inter Variable', -apple-system, system-ui, sans-serif",
+		},
+	},
+	proTheme
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider theme={theme} >
-    <App /> 
-  </ChakraProvider>,
-)
+	<ChakraProvider theme={theme}>
+		<App />
+	</ChakraProvider>
+);
